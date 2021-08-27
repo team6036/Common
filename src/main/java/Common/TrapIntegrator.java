83ddb62bb1d98366ba116f2c5d1847741dfa1281
BenMaydan <33559330+BenMaydan@users.noreply.github.com>
2021-08-26 17:54:15 -0700

    Create TrapIntegrator.java

---
+++ src/main/java/Common/TrapIntegrator.java
@@ -0,0 +1,19 @@
+public class TrapIntegrator {
+
+    public double preVel = 0;
+    public double newVel = 0;
+
+    public double pos;
+
+    public TrapIntegrator(double initPos){
+        pos = initPos;
+    }
+
+    public void update(double vel, double dt){
+        newVel = vel;
+
+        pos = pos + 0.5 * (newVel + preVel) * dt;
+
+        preVel = vel;
+    }
+}
